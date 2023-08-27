698cc85a436387f3c155d44d81ea4431d9249272
Jschles1 <johnschlesinger5@gmail.com>
2023-08-27 18:49:17 -0400

    Auth menu, invites and roadmaps menu

--- components/navigation/mobile-user-menu.tsx
+++ components/navigation/mobile-user-menu.tsx
@@ -1,31 +1,78 @@
-import Image from "next/image";
-import { Button } from "../ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui/sheet";
-import IconVerticalEllipsis from "public/images/icon-vertical-ellipsis.svg";
+// "use client";
 
-export default function MobileUserMenu() {
+import Image from "next/image";
+import { auth } from "@clerk/nextjs";
+import { Mail, Map } from "lucide-react";
+import { Button } from "../ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import IconVerticalEllipsis from "public/images/icon-vertical-ellipsis.svg";
+import { Roadmap } from "@prisma/client";
+import prismadb from "@/lib/prisma-db";
+
+interface MobileUserMenuProps {
+  roadmaps: Roadmap[];
+}
+
+function MenuButton({
+  children,
+  disabled,
+}: { children: React.ReactNode } & React.ComponentPropsWithoutRef<
+  typeof Button
+>) {
+  return (
+    <Button
+      disabled={disabled}
+      variant="ghost"
+      className="text-gray text-[0.938rem] disabled:bg-white disabled:text-gray bg-white rounded-tl-none rounded-bl-none rounded-tr-[100px] rounded-br-[100px] w-full justify-start hover:text-white focus:text-white focus-within:text-white focus:bg-dark-lavender active:text-white active:bg-dark-lavender"
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default async function MobileUserMenu({
+  roadmaps,
+}: MobileUserMenuProps) {
+  const { userId } = auth();
+
+  const roadmapInvites = await prismadb.roadmapInvite.findMany({
+    where: {
+      menteeId: userId,
+    },
+  });
+  const inviteLength = roadmapInvites?.length;
+  const disableInviteButton = !inviteLength;
+
+  // TODO: Show roadmaps that the user is a part of on non-home pages
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button className="ml-4" variant="ghost" size="icon">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="hover:bg-lighter-blue-gray focus:bg-lighter-blue-gray focus-within:bg-lighter-blue-gray focus-visible:bg-lighter-blue-gray active:bg-lighter-blue-gray"
+        >
           <Image src={IconVerticalEllipsis} alt="Menu" />
         </Button>
       </SheetTrigger>
-      <SheetContent className="bg-lighter-blue-gray">
-        <SheetHeader>
-          <SheetTitle>Main Menu</SheetTitle>
-          <SheetDescription>Description</SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">Hello</div>
-        <SheetFooter></SheetFooter>
+      <SheetContent className="bg-lighter-blue-gray py-4 pr-4 pl-0">
+        {/* TODO: Place in client component and change sheet content based on page */}
+        <div className="flex flex-col gap-4">
+          <MenuButton disabled={disableInviteButton}>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex items-center gap-x-4">
+                <Mail size={16} /> Roadmap Invites
+              </div>
+              <div>{inviteLength}</div>
+            </div>
+          </MenuButton>
+          <MenuButton>
+            <div className="flex items-center gap-x-4">
+              <Map size={16} /> All Roadmaps
+            </div>
+          </MenuButton>
+        </div>
       </SheetContent>
     </Sheet>
   );
--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,37 +1,20 @@
 // "use client";
 
-import { useParams, useSearchParams } from "next/navigation";
-import { auth, currentUser } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Logo from "public/images/logo-light.svg";
-import IconVerticalEllipsis from "public/images/icon-vertical-ellipsis.svg";
 
 import prismadb from "@/lib/prisma-db";
-import MobileRoadmapDropdownMenu from "./mobile-roadmap-dropdown-menu";
 import AddButton from "./add-button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui/sheet";
-import { Button } from "../ui/button";
+import MobileUserMenu from "./mobile-user-menu";
 
 export default async function Navigation() {
   const { userId } = auth();
-  const user = await currentUser();
-  // const roadmaps = await prismadb.roadmap.findMany({
-  //   where: {
-  //     menteeId: userId,
-  //   },
-  // });
-
-  console.log(userId);
-  console.log({ user });
-  // console.log({ roadmaps });
+  const roadmaps = await prismadb.roadmap.findMany({
+    where: {
+      menteeId: userId,
+    },
+  });
 
   return (
     <div className="h-16 py-5 px-4 bg-white flex items-center justify-between z-49">
@@ -40,26 +23,18 @@ export default async function Navigation() {
         <h1 className="text-[1.125rem] font-bold">Roadmap</h1>
       </div>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-x-4 md:hidden">
         <AddButton />
-
-        <div className="md:hidden">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button className="ml-4" variant="ghost" size="icon">
-                <Image src={IconVerticalEllipsis} alt="Menu" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent className="bg-lighter-blue-gray">
-              <SheetHeader>
-                <SheetTitle>Main Menu</SheetTitle>
-                <SheetDescription>Description</SheetDescription>
-              </SheetHeader>
-              <div className="grid gap-4 py-4">Hello</div>
-              <SheetFooter></SheetFooter>
-            </SheetContent>
-          </Sheet>
-        </div>
+        <UserButton
+          afterSignOutUrl="/sign-in"
+          appearance={{
+            elements: {
+              userButtonPopoverCard: "mx-auto !top-20 right-0 rounded-lg",
+            },
+          }}
+          userProfileMode="modal"
+        />
+        <MobileUserMenu roadmaps={roadmaps} />
       </div>
     </div>
   );
