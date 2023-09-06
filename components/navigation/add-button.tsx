4e1b31a4926b4066ae02b8e4b4ed493137bbb6e0
Jschles1 <johnschlesinger5@gmail.com>
2023-09-06 15:39:49 -0400

    Show dialog triggered by add button based on path

--- components/navigation/add-button.tsx
+++ components/navigation/add-button.tsx
@@ -3,12 +3,31 @@
 import { Button } from "../ui/button";
 import Image from "next/image";
 import AddTaskIcon from "public/images/icon-add-task-mobile.svg";
+import CreateRoadMapDialog from "../roadmaps/create-roadmap-dialog";
+import { usePathname } from "next/navigation";
+
+const WrapperComponentsByPage = {
+  "/roadmaps": (props: { trigger: React.ReactNode }) => (
+    <CreateRoadMapDialog {...props} />
+  ),
+};
 
 export default function AddButton() {
-  // TODO: Determine modal to open based on current page
+  const pathname = usePathname();
+  const WrapperComponent =
+    WrapperComponentsByPage[pathname as keyof typeof WrapperComponentsByPage];
+
+  if (!WrapperComponent) {
+    return null;
+  }
+
   return (
-    <Button variant="default" size="sm" className="w-12 h-8 rounded-3xl">
-      <Image src={AddTaskIcon} alt="Add Icon" />
-    </Button>
+    <WrapperComponent
+      trigger={
+        <Button variant="default" size="sm" className="w-12 h-8 rounded-3xl">
+          <Image src={AddTaskIcon} alt="Add Icon" />
+        </Button>
+      }
+    />
   );
 }
