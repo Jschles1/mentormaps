a6b7d9142efd5015a6d4996b950025247c3cb3b1
Jschles1 <johnschlesinger5@gmail.com>
2023-09-06 17:27:12 -0400

    Add link to RoadmapCard

--- components/roadmaps/roadmap-card.tsx
+++ components/roadmaps/roadmap-card.tsx
@@ -1,4 +1,5 @@
 import { Roadmap } from "@prisma/client";
+import Link from "next/link";
 import {
   Card,
   CardDescription,
@@ -11,20 +12,22 @@ interface RoadmapCardProps {
 }
 
 export default function RoadmapCard({ roadmap }: RoadmapCardProps) {
-  const { title, milestonesCompleted, milestonesTotal } = roadmap;
+  const { title, milestonesCompleted, milestonesTotal, id } = roadmap;
 
   const milestonesCompletedMessage = `${milestonesCompleted} of ${milestonesTotal} milestones completed`;
 
   return (
-    <Card className="px-4 py-6 border-0">
-      <CardHeader className="p-0">
-        <CardTitle className="text-[0.938rem] text-black-darkest">
-          {title}
-        </CardTitle>
-        <CardDescription className="text-xs text-gray">
-          {milestonesCompletedMessage}
-        </CardDescription>
-      </CardHeader>
-    </Card>
+    <Link href={`/roadmaps/${id}`}>
+      <Card className="px-4 py-6 border-0">
+        <CardHeader className="p-0">
+          <CardTitle className="text-[0.938rem] text-black-darkest">
+            {title}
+          </CardTitle>
+          <CardDescription className="text-xs text-gray">
+            {milestonesCompletedMessage}
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
