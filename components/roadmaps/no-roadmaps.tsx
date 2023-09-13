8547110e2c060b880324a9f2fa0b3c3ad763375f
Jschles1 <johnschlesinger5@gmail.com>
2023-09-13 15:40:51 -0400

    Fix CSS issue

--- components/roadmaps/no-roadmaps.tsx
+++ components/roadmaps/no-roadmaps.tsx
@@ -3,7 +3,7 @@ import CreateRoadMapDialog from "./create-roadmap-dialog";
 
 export default function NoRoadmaps() {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-[calc(100vh-8rem)]">
       <div className="text-center flex flex-col items-center gap-4 text-gray font-bold text-[1.125rem]">
         <p className="text-gray">
           You currently do not own or belong to any roadmaps.
