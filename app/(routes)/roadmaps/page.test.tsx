6dbf0c2eaac9e478fa5c512caa0625d0587226d7
Jschles1 <johnschlesinger5@gmail.com>
2023-10-10 16:47:53 -0400

    No roadmaps test

--- app/(routes)/roadmaps/page.test.tsx
+++ app/(routes)/roadmaps/page.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { AppWrapper } from "@/__mocks__/wrappers";
 import { mockMentorRoadmaps, mockMenteeRoadmaps } from "@/__mocks__/roadmaps";
 import Page from "./page";
@@ -14,5 +14,20 @@ describe("Roadmaps Page", () => {
     const Result = await Page();
     const { baseElement } = render(Result, { wrapper: AppWrapper });
     expect(baseElement).toBeTruthy();
+  });
+
+  it("Should render successfully with no roadmaps", async () => {
+    jest.mock("@/lib/server/api/getRoadmaps", () => {
+      return jest.fn(() => ({
+        mentorRoadmaps: [],
+        menteeRoadmaps: [],
+      }));
+    });
+    const Result = await Page();
+    const { baseElement } = render(Result, { wrapper: AppWrapper });
+
+    expect(
+      screen.getByText("You currently do not own or belong to any roadmaps.")
+    ).toBeInTheDocument();
   });
 });
