913c3a6757aedf01e144bebd7657697772593157
Jschles1 <johnschlesinger5@gmail.com>
2023-10-27 10:16:13 -0400

    Fix roadmaps page mocking

--- __mocks__/wrappers.tsx
+++ __mocks__/wrappers.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const queryClient = new QueryClient({
+export const testQueryClient = new QueryClient({
   logger: {
     log: console.log,
     warn: console.warn,
@@ -12,6 +12,8 @@ const queryClient = new QueryClient({
 
 export const AppWrapper = ({ children }: { children: React.ReactNode }) => (
   <ClerkProvider>
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={testQueryClient}>
+      {children}
+    </QueryClientProvider>
   </ClerkProvider>
 );
--- app/(routes)/roadmaps/page.test.tsx
+++ app/(routes)/roadmaps/page.test.tsx
@@ -2,30 +2,41 @@ import { render, screen } from "@testing-library/react";
 import { AppWrapper } from "@/__mocks__/wrappers";
 import { mockMentorRoadmaps, mockMenteeRoadmaps } from "@/__mocks__/roadmaps";
 import Page from "./page";
+import getRoadmaps from "@/lib/server/api/getRoadmaps";
+import { testQueryClient } from "@/__mocks__/wrappers";
+
+jest.mock("@/lib/server/api/getRoadmaps");
 
 describe("Roadmaps Page", () => {
+  beforeEach(() => {
+    testQueryClient.clear();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // Restore all mocks back to their original implementations
+  });
+
   it("Should render successfully", async () => {
-    jest.mock("@/lib/server/api/getRoadmaps", () => {
-      return jest.fn(() => ({
-        mentorRoadmaps: mockMentorRoadmaps,
+    (getRoadmaps as jest.Mock).mockReturnValueOnce(
+      Promise.resolve({
         menteeRoadmaps: mockMenteeRoadmaps,
-      }));
-    });
+        mentorRoadmaps: mockMentorRoadmaps,
+      })
+    );
     const Result = await Page();
-    const { baseElement } = render(Result, { wrapper: AppWrapper });
-    expect(baseElement).toBeTruthy();
+    render(Result, { wrapper: AppWrapper });
+    expect(screen.getByText("Mentor Roadmaps (2)")).toBeInTheDocument();
   });
 
   it("Should render successfully with no roadmaps", async () => {
-    jest.mock("@/lib/server/api/getRoadmaps", () => {
-      return jest.fn(() => ({
-        mentorRoadmaps: [],
+    (getRoadmaps as jest.Mock).mockReturnValueOnce(
+      Promise.resolve({
         menteeRoadmaps: [],
-      }));
-    });
+        mentorRoadmaps: [],
+      })
+    );
     const Result = await Page();
     render(Result, { wrapper: AppWrapper });
-
     expect(
       screen.getByText("You currently do not own or belong to any roadmaps.")
     ).toBeInTheDocument();
